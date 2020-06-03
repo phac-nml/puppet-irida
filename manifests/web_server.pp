08e340ffd0e6f6dfb485c68a696d27dde8933974
Matt <matthew.gopez@canada.ca>
2020-06-03 16:13:28 -0500

    Param re-order

--- manifests/web_server.pp
+++ manifests/web_server.pp
@@ -5,10 +5,10 @@
 # @example
 #   include irida::web_server
 class irida::web_server (
-  String  $irida_ip_addr = $ipaddress,
   Boolean $apache_use_ssl,
   String  $cert_file_path,
   String  $cert_key_file_path,
+  String  $irida_ip_addr = $ipaddress,
 ) {
 
   package { 'mod_ssl':
